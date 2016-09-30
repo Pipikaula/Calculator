703768ab359d5206f919d343d353d829dc554a2c
Pipikaula <firethunderwater@yahoo.com>
2016-09-29 14:19:14 -1000

    Update ScientificCalculator.java

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -6,13 +6,13 @@
 package scientificcalculator;
 
 /***********************************************************
-*Calculator.java
+*ScientificCalculator.java
 *
-*A basic simple easy to use Calculator
+*Its a Scientific calculator but I removed the number buttons
 *
 *@Author: Andy Omori
 *
-*@Version: 9/17/13
+*@Version: 9/29/2016
 ************************************************************/
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -39,16 +39,7 @@ public class ScientificCalculator extends Application {
     
     String op = null;
     TextField tf = new TextField();
-    Button btn00 = new Button("0");
-    Button btn11 = new Button("1");
-    Button btn22 = new Button("2");
-    Button btn33 = new Button("3");
-    Button btn44 = new Button("4");
-    Button btn55 = new Button("5");
-    Button btn66 = new Button("6");
-    Button btn77 = new Button("7");
-    Button btn88 = new Button("8");
-    Button btn99 = new Button("9");
+
     Button btn1 = new Button("+");
     Button btn2 = new Button("-");
     Button btn3 = new Button("X");
@@ -85,7 +76,7 @@ public class ScientificCalculator extends Application {
     column2.setHalignment(HPos.LEFT);
     root.getColumnConstraints().add(column2);
     
-=
+    
     GridPane.setConstraints(tf, 2, 1);
     GridPane.setConstraints(btn1, 3, 4);
     GridPane.setConstraints(btn2, 1, 3);
@@ -95,16 +86,7 @@ public class ScientificCalculator extends Application {
     GridPane.setConstraints(btn6, 1, 7);
     GridPane.setConstraints(btn7, 1, 8);
 
-    GridPane.setConstraints(btn00, 3, 4);
-    GridPane.setConstraints(btn11, 2, 2);
-    GridPane.setConstraints(btn22, 3, 2);
-    GridPane.setConstraints(btn33, 4, 2);
-    GridPane.setConstraints(btn44, 2, 3);
-    GridPane.setConstraints(btn55, 3, 3);
-    GridPane.setConstraints(btn66, 4, 3);
-    GridPane.setConstraints(btn77, 2, 4);
-    GridPane.setConstraints(btn88, 3, 4);
-    GridPane.setConstraints(btn99, 4, 4);
+
     
     
     
@@ -122,17 +104,6 @@ public class ScientificCalculator extends Application {
     root.getChildren().addAll(btn5);
     root.getChildren().addAll(btn6);
     root.getChildren().addAll(btn7);
- 
-    root.getChildren().addAll(btn00);
-    root.getChildren().addAll(btn11);
-    root.getChildren().addAll(btn22);
-    root.getChildren().addAll(btn33);
-    root.getChildren().addAll(btn44);
-    root.getChildren().addAll(btn55);
-    root.getChildren().addAll(btn66);
-    root.getChildren().addAll(btn77);
-    root.getChildren().addAll(btn88);
-    root.getChildren().addAll(btn99);
     
  
     root.getChildren().addAll(tf);  
@@ -155,16 +126,7 @@ public class ScientificCalculator extends Application {
     btn5.setOnAction(new MyHandler());
     btn6.setOnAction(new MyHandler());
     btn7.setOnAction(new MyHandler());
-    btn00.setOnAction(new MyHandler());
-    btn11.setOnAction(new MyHandler());
-    btn22.setOnAction(new MyHandler());
-    btn33.setOnAction(new MyHandler());
-    btn44.setOnAction(new MyHandler());
-    btn55.setOnAction(new MyHandler());
-    btn66.setOnAction(new MyHandler());
-    btn77.setOnAction(new MyHandler());
-    btn88.setOnAction(new MyHandler());
-    btn99.setOnAction(new MyHandler());
+
     
     
    
@@ -275,7 +237,7 @@ public class ScientificCalculator extends Application {
    }
 }   
 
-
+/*
    public String Date(String date) {
       Date today = new Date();
       DateFormat df = DateFormat.getDateInstance(DateFormat.DEFAULT, Locale.US);
@@ -283,7 +245,6 @@ public class ScientificCalculator extends Application {
       setText(strDate);
     }
 
-
-
+*/
 
 
