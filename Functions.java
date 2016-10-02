e041b082815639839bf7d8511c84f6e87d8f4cc4
Pipikaula <firethunderwater@yahoo.com>
2016-10-01 15:46:04 -1000

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package scientificcalculator;
 
 /**
- *
- * @author PC
+ * All of the function for the calculator
+ * Andy Omori
  */
 public class Functions extends ScientificCalculator  {
     
